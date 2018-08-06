8606c63425217534eef25bf3180d86552e4a6556
Zachary Marcus <zachary.j.marcus@gmail.com>
2018-08-05 23:39:05 -0400

    added baseline server

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var session = require('express-session');
+var bodyParser = require('body-parser');
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(session({
+  resave: false,
+  saveUnitialized: true,
+  secret: 'my secret though yo'
+}));
+
+//const mongoose = require('mongoose');
+//mongoose.connect('mongodb://localhost/webdev_project_server');
+
+
+
+const newsFeedService = require('./services/new-feed.service.server');
+newsFeedService(app);
+
+app.listen(3000);
+
+
