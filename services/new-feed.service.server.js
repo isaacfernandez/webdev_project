f45c8a8baa1153d5bdd12ecbca2bb02468926d31
Zachary Marcus <zachary.j.marcus@gmail.com>
2018-08-05 23:46:19 -0400

    pipes work I guess

--- services/new-feed.service.server.js
+++ services/new-feed.service.server.js
@@ -1,5 +1,6 @@
 
 var reqs = require('request-promise');
+var request = require('request');
 
 var newsApi = 'https://newsapi.org/v2/top-headlines?sources=google-news&apiKey=7dd275983bdb4bbea426b022d69d76f3';
 
@@ -9,15 +10,18 @@ module.exports = app => {
 
   function getAllWhatAmIDoingNews(req, res) {
     var response = 'Hello Express app!\n';
-    reqs(newsApi).then(function(response) {
-      console.log(response);
-      return response.articles;
-    })
-    .then(function(news) {
-      console.log(news);
-      res.json(news);//res.send(news);
-      console.log('bogus');
-    });
+    req.pipe(request(newsApi)).pipe(res);
   }
+
+//    reqs(newsApi).then(function(response) {
+//      console.log(response);
+//      return response.articles;
+//    })
+//    .then(function(news) {
+//      console.log(news);
+//      res.json(news);//res.send(news);
+//      console.log('bogus');
+//    });
+//  }
 
 };
