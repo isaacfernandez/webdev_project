a114d2e4665019a31130cc6cd3ff7dbced215dd1
Zachary Marcus <zachary.j.marcus@gmail.com>
2018-08-09 14:19:02 -0400

    added a user schema

---
+++ models/user/user.schema.server.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+
+
+module.exports = mongoose.Schema({
+  username: String,
+  password: String,
+  firstName: String,
+  lastName: String
+  dateOfBirth: {
+    type: Date,
+    default: Date.now
+  }
+  userFollows: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user-follow'
+  }],
+  feedFollows: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'feed-follow'
+  }]
+}, {collection: 'post'});
+
+
+
