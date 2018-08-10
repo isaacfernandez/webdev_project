d128654325d4d140fefd856430e7b20a42e99560
Zachary Marcus <zachary.j.marcus@gmail.com>
2018-08-10 18:24:38 -0400

    updated server.js file with proper permissions and env vars, and added structure for real service files

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://cs4550-bk610-project-client.herokuapp.com');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
@@ -19,14 +19,30 @@ app.use(session({
   secret: 'my secret though yo'
 }));
 
-//const mongoose = require('mongoose');
-//mongoose.connect('mongodb://localhost/webdev_project_server');
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI);
 
-
-
-const newsFeedService = require('./services/new-feed.service.server');
-newsFeedService(app);
+require('./services/post.service.server')(app);
+require('./services/feed.service.server')(app);
+require('./services/feed-follow.service.server')(app);
+require('./services/user.service.server')(app);
+require('./services/user-follow.service.server')(app);
 
 app.listen(process.env.PORT || 3000);
 
+function setSession(request, res) {
+  var name = request.params.name;
+  var val = request.params.value;
+  request.session[name] = val;
+  res.send(request.session);
+}
+
+function getSession(request, res) {
+  var name = request.params.name;
+  var val = request.session[name];
+  res.send(val);
+}
+
+app.get('/api/session/set/:name/:value', setSession);
+app.get('/api/session/get/:name', getSession);
 
