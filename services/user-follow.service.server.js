cb76635fbf8e28b7372bff2ae075026d7a17dd17
Zachary Marcus <zachary.j.marcus@gmail.com>
2018-08-16 17:14:19 -0400

    added the api to adhere to for user follows

---
+++ services/user-follow.service.server.js
@@ -0,0 +1,20 @@
+
+module.exports = function(app) {
+
+  app.post('/api/user/:userId/follows/:followedId', followUser);
+  app.get('/api/user/:userId/follows/:quantity', getFollows);
+  app.get('/api/user/:userId/follower/:quantity', getFollowers);
+  app.get('/api/user/:userId/isFollowing/:followingId', isFollowing);
+  app.delete('/api/user/:userId/follows/:followedId', unfollowUser);
+
+  function followUser(req, res) {}
+  function getFollows(req, res) {}
+  function getFollowers(req, res) {}
+  function isFollowing(req, res) {}
+  function unfollowUser(req, res) {}
+
+}
+
+
+
+
