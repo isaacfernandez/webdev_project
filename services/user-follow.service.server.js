e227173a1493436a0cd1124254f50c810ce17eab
Zachary Marcus <zachary.j.marcus@gmail.com>
2018-08-17 17:40:39 -0400

    implemented user follow API

--- services/user-follow.service.server.js
+++ services/user-follow.service.server.js
@@ -6,6 +6,7 @@ module.exports = function(app) {
   app.get('/api/user/:userId/follower/:quantity', getFollowers);
   app.get('/api/user/:userId/isFollowing/:followingId', isFollowing);
   app.delete('/api/user/:userId/follows/:followedId', unfollowUser);
+  app.delete('/api/user-follow/:userFollowId', unfollowUserById);
 
   var userFollowModel = require('../models/user-follow/user-follow.model.server');
   // schema: follower, followed, followingSince
@@ -22,19 +23,50 @@ module.exports = function(app) {
   }
 
   function getFollows(req, res) {
-    
+    userFollowModel.findUserFollowsForFollower(req.params['userId'])
+      .sort({'followingSince': -1})
+      .limit(req.params['quantity'])
+      .then(function(follows) {
+        res.send(follows);
+      });
   }
 
   function getFollowers(req, res) {
-    
+    userFollowModel.findUserFollowsForFollowed(req.params['userId'])
+      .sort({'followingSince': -1})
+      .limit(req.params['quantity'])
+      .then(function(follows) {
+        res.send(follows);
+      });
   }
 
   function isFollowing(req, res) {
-    
+    userFollowModel.doesUserFollowOther(
+      req.params['userId'],
+      req.params['followingId'])
+      .then(function(potentialFollow) {
+        if (potentialFollow !== null) {
+          res.json({'response': true});
+        } else {
+          res.json({'response': false});
+        }
+      });
   }
 
   function unfollowUser(req, res) {
-    
+    userFollowModel.deleteUserFollowByFollowerAndFollowed(
+      req.params['userId'],
+      req.params['followedId'])
+      .then(function(response) {
+        res.send(response)
+      });
+  }
+
+  function unfollowUserById(req, res) {
+    userFollowModel.deleteUserFollowById(req.params['userFollowId'])
+      .then(function(response) {
+        res.send(response)
+      });
   }
 
 }
