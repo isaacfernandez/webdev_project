da49258ba05d16be5b78a5459cdfe58577a1a86d
Zachary Marcus <zachary.j.marcus@gmail.com>
2018-08-17 19:06:39 -0400

    added some more methods

--- models/user-follow/user-follow.model.server.js
+++ models/user-follow/user-follow.model.server.js
@@ -15,8 +15,16 @@ function deleteUserFollowById(idToDelete) {
   return model.remove({_id: idToDelete});
 }
 
+function deleteUserFollowByFollowerAndFollowed(follower, followed) {
+  return model.remove({follower: follower, followed: followed});
+}
+
 function findUserFollowById(idToFind) {
   return model.findById(idToFind);
+}
+
+function doesUserFollowOther(userId, otherId) {
+  return model.find({follower: userId, followed: otherId});
 }
 
 function findUserFollowsForFollowed(followedId) {
@@ -36,7 +44,9 @@ module.exports = {
   createUserFollow: createUserFollow,
   updateUserFollow: updateUserFollow,
   deleteUserFollowById: deleteUserFollowById,
+  deleteUserFollowByFollowerAndFollowed: deleteUserFollowByFollowerAndFollowed,
   findUserFollowById: findUserFollowById,
+  doesUserFollowOther: doesUserFollowOther,
   findUserFollowsForFollowed: findUserFollowsForFollowed,
   findUserFollowsForFollower: findUserFollowsForFollower,
   findAllUserFollows: findAllUserFollows
