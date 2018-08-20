d9168bb0517d520d5ae15aaa9a5dcb1e7c2aee48
Zachary Marcus <zachary.j.marcus@gmail.com>
2018-08-19 21:05:20 -0400

    added more callback hell logging

--- models/user/user.model.server.js
+++ models/user/user.model.server.js
@@ -69,7 +69,19 @@ function removeFeedFollowById(userId, feedFollowId) {
 
 function addPostToUser(userId, postId) {
   console.log('in addPostToUser (userModel)');
-  return model.update({_id: userId}, {$push: {posts: postId}}, function(error){console.log(error);});
+  return model.update(
+    {_id: userId},
+    {$push: {posts: postId}},
+    function(error, success){
+      if (error) {
+        console.log('error');
+        console.log(error);
+      } else {
+        console.log('addPostToUser success');
+        console.log(success);
+        return success;
+      }
+    });
 }
 
 function removePostFromUser(userId, postId) {
